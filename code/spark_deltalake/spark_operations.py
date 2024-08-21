aa97f7d73b835e3ca7a29f0e4124956029c62c3f
Lukasz <prosowiec@gmail.com>
2024-08-21 23:06:17 +0200

    Add checks file/spark

--- code/spark_deltalake/spark_operations.py
+++ code/spark_deltalake/spark_operations.py
@@ -5,28 +5,9 @@ from dotenv import load_dotenv
 import os
 import time
 from .sparkschema import schemaSelect
+from .checks import chceck_CloudStorageAccount, chceck_hadoop_azure
 
-def chceck_hadoop_azure(spark):
-    check = 1
-    try:
-        spark._jvm.Class.forName("org.apache.hadoop.fs.azure.NativeAzureFileSystem")
-        print("hadoop-azure is available")
-    except Exception as e:
-        print("hadoop-azure is NOT available")
-        check = 0
-        
-    return check
 
-def chceck_CloudStorageAccount(spark):
-    check = 1
-    try:
-        spark._jvm.Class.forName("com.microsoft.azure.storage.CloudStorageAccount")
-        print("azure-storage is available")
-    except Exception as e:
-        print("azure-storage is NOT available")
-        check = 0
-        
-    return check
 
 
 
